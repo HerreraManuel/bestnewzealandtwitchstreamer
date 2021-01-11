a203d22f001b05eebe48e66f5e6230426cf8c8c5
HerreraManuel <mherrera.jm@gmail.com>
2021-01-11 02:58:45 -0800

    change background

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={chiblee} className="App-logo" alt="logo"
+        <a href="https://www.twitch.tv/chiblee"><img src={chiblee} className="App-logo" alt="Chiblee-logo"
           style={{
             width: 200,
             height: 200,
             borderRadius: 200 / 2
           }}
-       />
+       /></a>
         <a
           className="App-link"
           href="https://www.twitch.tv/chiblee"
